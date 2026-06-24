63beada7e29863ec146061f687259e243c172d37
LeonardoSaintAna <88788079+LeonardoSaintAna@users.noreply.github.com>
2026-06-24 15:01:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,28 +1,33 @@
 import flet as ft
 
-
 def main(page: ft.Page):
     page.title = "Meu Primeiro App"
-    page.theme_mode = ft.ThemeMode.LIGHT
+    page.padding = 16
 
     texto = ft.Text(
-        value="Olá Mundo!",
-        size=30
+        "Olá Mundo!",
+        size=32,
+        weight=ft.FontWeight.BOLD
     )
 
     def mudar_texto(e):
         texto.value = "Botão clicado!"
         page.update()
 
-    botao = ft.ElevatedButton(
-        text="Clique Aqui",
-        on_click=mudar_texto
-    )
-
     page.add(
-        texto,
-        botao
+        ft.Column(
+            [
+                texto,
+                ft.ElevatedButton(
+                    "Clique Aqui",
+                    on_click=mudar_texto,
+                    expand=True
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=True,
+        )
     )
-
 
 ft.app(target=main)
